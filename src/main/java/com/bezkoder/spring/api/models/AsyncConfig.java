fc9b14456d5b54974b653188f906bb557db090a0
Usman Anwar <usmananwar4all@gmail.com>
2023-07-15 03:22:21 +0900

    aa

--- src/main/java/com/bezkoder/spring/api/models/AsyncConfig.java
+++ src/main/java/com/bezkoder/spring/api/models/AsyncConfig.java
@@ -1,26 +1,26 @@
-package com.bezkoder.spring.api.models;
-
-import java.util.concurrent.Executor;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-@Configuration
-@EnableAsync
-public class AsyncConfig {
-
-    @Bean
-    public Executor asyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(100);
-        executor.setQueueCapacity(10);
-        executor.setThreadNamePrefix("AsyncThread-");
-        executor.initialize();
-        return executor;
-    }
-
-    // Other configuration beans and settings
-}
+//package com.bezkoder.spring.api.models;
+//
+//import java.util.concurrent.Executor;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//@Configuration
+//@EnableAsync
+//public class AsyncConfig {
+//
+//    @Bean
+//    public Executor asyncExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(10);
+//        executor.setMaxPoolSize(100);
+//        executor.setQueueCapacity(10);
+//        executor.setThreadNamePrefix("AsyncThread-");
+//        executor.initialize();
+//        return executor;
+//    }
+//
+//    // Other configuration beans and settings
+//}
